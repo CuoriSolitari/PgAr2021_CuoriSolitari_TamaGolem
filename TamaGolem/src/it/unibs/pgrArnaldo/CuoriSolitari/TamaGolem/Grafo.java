dc692ab97b72388fddbc2870377e2081e9089610
paoloformentelli <p.formentelli001@studenti.unibs.it>
2021-05-11 23:28:36 +0200

    Grafi

---
+++ TamaGolem/src/it/unibs/pgrArnaldo/CuoriSolitari/TamaGolem/Grafo.java
@@ -0,0 +1,9 @@
+package it.unibs.pgrArnaldo.CuoriSolitari.TamaGolem;
+
+public class Grafo {
+
+    public void creaGrafo(Nodo n1, Nodo n2, Nodo n3, Arco a1, Arco a2, Arco a3){
+
+
+    }
+}
---
+++ TamaGolem/src/it/unibs/pgrArnaldo/CuoriSolitari/TamaGolem/Nodo.java
@@ -0,0 +1,10 @@
+package it.unibs.pgrArnaldo.CuoriSolitari.TamaGolem;
+
+public class Nodo {
+
+    private Elemento e;
+
+    public Nodo(Elemento _e) {
+        this.e = _e;
+    }
+}
